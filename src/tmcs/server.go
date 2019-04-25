ab3b4b51906bd25bd7358206b3ecdf03d6b1ce97
SardineFish <Sardine@live.cn>
2019-04-25 15:44:35 +0800

    Fix bugs of TLS server.

--- src/tmcs/server.go
+++ src/tmcs/server.go
@@ -72,9 +72,9 @@ func NewTMCSAnonymousServer(tmcs *TMCSAnonymous, config *tmcs_config.TMCSConfig)
 		MinVersion:   tls.VersionTLS12,
 		CipherSuites: []uint16{
 			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
 		},
 	}
 	return server, nil
