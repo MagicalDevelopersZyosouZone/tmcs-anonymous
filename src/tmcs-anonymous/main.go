561de77760e1548b755e41d080c3d6a448247bc4
SardineFish <sardine@live.cn>
2019-05-04 23:44:57 +0800

    Add cli args for tmcs-anonymous server program.

--- src/tmcs-anonymous/main.go
+++ src/tmcs-anonymous/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	tmcs_config "config"
+	"flag"
 	"os"
 	"serverlog"
 	"tmcs"
@@ -13,6 +14,19 @@ func main() {
 		serverlog.Error(err.Error())
 		os.Exit(1)
 	}
+
+	addr := flag.String("addr", config.Address, "The address to setup http server.")
+	tls := flag.Bool("tls", config.TLS, "Use TLS connection.")
+	cert := flag.String("cert", config.Cert, "Path of certificate file used for TLS.")
+	certKey := flag.String("cert-key", config.CertKey, "Path of certificate key file.")
+
+	flag.Parse()
+
+	config.Address = *addr
+	config.TLS = *tls
+	config.Cert = *cert
+	config.CertKey = *certKey
+
 	tmcs := tmcs.NewTMCSAnonymous(config)
 	tmcs.Start()
 }
